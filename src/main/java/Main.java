dfaaa4f1eaee49daf5ad714e3d3d55c185e4f4fc
MarkusDrejer <mabd412b@stud.kea.dk>
2019-02-21 15:37:05 +0100

    Added Dependecies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
     }
 
